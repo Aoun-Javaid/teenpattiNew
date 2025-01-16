4a0176d18e5e75ae8cf2e2487706087f46fe5218
Aoun-Universal <78606381+Aoun-Javaid@users.noreply.github.com>
2025-01-16 13:09:16 +0400

    chat

--- src/app/shared/chat/chat.component.ts
+++ src/app/shared/chat/chat.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ToggleService } from '../../services/toggle.service';
@@ -6,36 +7,53 @@ import { WebSocketService } from '../../services/web-socket.service';
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent implements OnInit {
-  text:any;
+  text: any;
+  casinoChat: any = [];
+  connectedUsers: any;
   constructor(private toggle: ToggleService, private socketService: WebSocketService) {
 
   }
   ngOnInit(): void {
 
-    this.socketService.onEvent('userTest', (data) => {
-      console.log('Received userTest event:', data);
+    this.socketService.onEvent('loadConnectedClients', (data) => {
+      console.log('Received loadConnectedClients:', data);
+      this.connectedUsers = data;
     });
 
-    this.socketService.onEvent('message', (data) => {
+    this.socketService.onEvent('loadNewMessage', (data) => {
+      this.updateIncomingMessage(data);
       console.log('Received message event:', data);
     });
-    this.socketService.listenForMessages().subscribe((data: any) => {
-      if (data) {
-        console.log('Received messages:', data);
-        // this.updateIncomingMessage(data);
-      }
-    });
+    // this.socketService.listenForMessages().subscribe((data: any) => {
+    //   if (data) {
+    //     console.log('Received messages:', data);
+    //     // this.updateIncomingMessage(data);
+    //   }
+    // });
   }
   closeMobSideBar() {
     this.toggle.setMobSideBarState(false);
   }
   sendMessage() {
-    this.socketService.sendMessage('message', { content: this.text });
-    this.text='';
+    if (this.text != '') {
+      this.socketService.sendMessage('newMessage', { content: this.text });
+      this.text = '';
+    }
+  }
+  updateIncomingMessage(data: any) {
+    this.casinoChat.push(data);
+    setTimeout(() => {
+      const parent = document.querySelector('.messages');
+      const lastChild = parent?.lastElementChild;
+
+      if (lastChild) {
+        lastChild.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+      }
+    }, 200);
   }
 }
