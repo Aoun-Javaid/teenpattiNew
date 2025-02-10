33cf6eca2553d4b28d05cbde4d0f9a9e854af495
hinaasghar005 <hinaasghar005@gmail.com>
2025-02-10 23:03:23 +0500

    integrated timer in Vedio player

--- src/app/shared/video-player-unreal/video-player-unreal.component.ts
+++ src/app/shared/video-player-unreal/video-player-unreal.component.ts
@@ -154,7 +154,7 @@ export class VideoPlayerUnrealComponent implements OnDestroy,OnInit{
   // Initially, no time has passed, but this will count up
   // and subtract from the TIME_LIMIT
   timePassed = 0;
-  timeLeft = 0;
+  // timeLeft = 0;
   // config: CountdownConfig ;
   config: CountdownConfig = {
     formatDate: ({ date }) => `${date / 1000}`,
@@ -253,6 +253,7 @@ export class VideoPlayerUnrealComponent implements OnDestroy,OnInit{
   }
 
   ngOnInit(): void {
+    this.startTimer();
     // this.surpriseFireWork()
     // this.surprise()
     if (this.eventId != '99.0062') {
@@ -706,6 +707,54 @@ export class VideoPlayerUnrealComponent implements OnDestroy,OnInit{
     }, 250);
   }
 
+
+  // timer
+  timeLeft: number = 19;
+  interval: any;
+  animate: boolean = false;
+
+  // ngOnInit() {
+  //   this.startTimer();
+  // }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.triggerAnimation();
+        this.timeLeft--;
+      } else {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  triggerAnimation() {
+    this.animate = false;
+    setTimeout(() => {
+      this.animate = true;
+    }, 50);
+  }
+
+  getStrokeDasharray(): string {
+    const circumference = 2 * Math.PI * 45;
+    const progress = (circumference * this.timeLeft) / 29;
+    return `${progress} ${circumference}`;
+  }
+
+  getStrokeColor(): string {
+    if (this.timeLeft <= 3) {
+      return '#FF0000';
+    } else if (this.timeLeft === 4) {
+      return '#FF4500';
+    } else if (this.timeLeft <= 5) {
+      return '#FEFF00';
+    }
+    return '#AAFF00';
+  }
+
+  getOpacity(): number {
+    return this.timeLeft > 4 ? 1 : 0.5;
+  }
 }
 declare function RunPlayer(
   id: string,
