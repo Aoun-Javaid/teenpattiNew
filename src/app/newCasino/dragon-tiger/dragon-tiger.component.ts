0a4d9fcd4e3dbfcaef5de2ccd164a1b1e053d2d9
waseemahmad <waseemahmedofficial2000@gmail.com>
2025-02-16 11:12:55 +0500

    finalize the changes regarding design part

--- src/app/newCasino/dragon-tiger/dragon-tiger.component.ts
+++ src/app/newCasino/dragon-tiger/dragon-tiger.component.ts
@@ -2,13 +2,15 @@ import {Component, HostListener, OnInit} from '@angular/core';
 import {TopResultsComponent} from "../shared/top-results/top-results.component";
 import {VideoPlayerComponent} from "../../shared/video-player/video-player.component";
 import {CommonModule} from "@angular/common";
+import {BetCoinComponent} from "../../shared/bet-coin/bet-coin.component";
+import {ShortNumberPipe} from "../../pipes/short-number.pipe";
 
 @Component({
   selector: 'app-dragon-tiger',
   standalone: true,
   imports: [
     TopResultsComponent,
-    VideoPlayerComponent, CommonModule
+    VideoPlayerComponent, CommonModule, BetCoinComponent, ShortNumberPipe
   ],
   templateUrl: './dragon-tiger.component.html',
   styleUrl: './dragon-tiger.component.css'
@@ -18,6 +20,8 @@ export class DragonTigerComponent implements OnInit {
   RoundWinner: any;
   isDesktop!: boolean;
   marketArray: any;
+  game: any;
+  winnerMarketArray: any;
 
   ngOnInit() {
     this.getWindowSize()
