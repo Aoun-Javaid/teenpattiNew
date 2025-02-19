15a073f1e8d7dc514f86f260a5455534c88f7c3e
faizan99904 <faizan99904@gmail.com>
2025-02-19 12:33:04 +0500

    fix mobile view

--- src/app/newCasino/sic-bo/sic-bo.component.ts
+++ src/app/newCasino/sic-bo/sic-bo.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { VideoPlayerComponent } from "../../shared/video-player/video-player.component";
+import { TopResultsComponent } from '../shared/top-results/top-results.component';
 
 @Component({
   selector: 'app-sic-bo',
   standalone: true,
-  imports: [],
+  imports: [VideoPlayerComponent, TopResultsComponent],
   templateUrl: './sic-bo.component.html',
   styleUrl: './sic-bo.component.css'
 })
 export class SicBoComponent {
- 
+  RoundWinner: any;
 }
