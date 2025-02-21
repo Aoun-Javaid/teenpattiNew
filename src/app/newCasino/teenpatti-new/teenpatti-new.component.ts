00f1b077f59467ae940ed126db888230eefe5b36
Aoun-Universal <78606381+Aoun-Javaid@users.noreply.github.com>
2025-02-21 09:47:22 +0500

    cc

--- src/app/newCasino/teenpatti-new/teenpatti-new.component.ts
+++ src/app/newCasino/teenpatti-new/teenpatti-new.component.ts
@@ -177,7 +177,7 @@ export class TeenpattiNewComponent implements OnInit, OnDestroy {
         // this.getRoundId = localStorage.getItem('roundID')
 
         let objMarket = JSON.parse(marketData);
-        console.log('market data', objMarket)
+        // console.log('market data', objMarket)
         // let objMarket = marketData;
         if (this.eventid == '99.0046') {
           // console.log(objMarket)
