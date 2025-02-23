88abaa5d5f67b87a53e6d7cc233d9df048b0c90b
waseemahmad <waseemahmedofficial2000@gmail.com>
2025-02-23 12:42:12 +0500

    created virtual dragon tiger and virtual casino folder

---
+++ src/app/virtualCasino/virtual-dt/virtual-dt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-dt',
+  standalone: true,
+  imports: [],
+  templateUrl: './virtual-dt.component.html',
+  styleUrl: './virtual-dt.component.css'
+})
+export class VirtualDtComponent {
+
+}
