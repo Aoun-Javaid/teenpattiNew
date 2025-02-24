78d853f44a5cf4e1101ef51ed2157b54c5d5bbc9
waseemahmad <waseemahmedofficial2000@gmail.com>
2025-02-24 14:22:09 +0500

    changes

--- src/app/virtualCasino/virtual-dt/virtual-dt.component.ts
+++ src/app/virtualCasino/virtual-dt/virtual-dt.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {TimerComponent} from "../shared/timer/timer.component";
+import {TopResultsComponent} from "../../newCasino/shared/top-results/top-results.component";
 
 @Component({
   selector: 'app-virtual-dt',
   standalone: true,
-  imports: [
-    TimerComponent
-  ],
+    imports: [
+        TimerComponent,
+        TopResultsComponent
+    ],
   templateUrl: './virtual-dt.component.html',
   styleUrl: './virtual-dt.component.css'
 })
