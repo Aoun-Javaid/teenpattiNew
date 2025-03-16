95fed144e982162da63a7421d9972e3acfa1468d
Aoun-Universal <78606381+Aoun-Javaid@users.noreply.github.com>
2025-03-16 11:55:30 +0500

    changes

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 
 
 // const BASE_URL = 'http://101.0.20.58:5011';
-const BASE_URL = 'https://universestudio.online';
+const BASE_URL = 'https://v2.universestudio.online';
 
 // const BASE_URL = '';
 export const BASE_URL_WS = "https://universestudio.online";
@@ -41,6 +41,8 @@ export const CONFIG = {
   virtualCasinoPlacebetURL: BASE_URL + "/casinoapp/exchange/users/casino/placebet",
   getUserBalanceURL: BASE_URL + "/v2/operator/user/userBalance",
   getAllMarketplURL: BASE_URL + '/v2/operator/user/userBetProfitLoss',
+
+  authlaunch: BASE_URL + "/v2/operator/auth",
 };
 
 
---
+++ src/app/auth/launcher/launcher.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first, Subscription } from 'rxjs';
+import { CONFIG } from '../../../../config';
+import { NetworkService } from '../../services/network.service';
+
+@Component({
+  selector: 'app-launcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './launcher.component.html',
+  styleUrl: './launcher.component.css'
+})
+export class LauncherComponent implements OnInit,OnDestroy{
+  userToken: any;
+  systemAuthToken: any;
+  operator: any;
+  eventId: any;
+
+  routeSub!: Subscription;
+  public tableId: any;
+  public accessToken: any;
+  roomId: any;
+  header: string | null = '';
+  themeConfigData: any;
+  site: string = '';
+  isLoader = false
+
+  favIcon: HTMLLinkElement = document.querySelector('#appIcon') as HTMLLinkElement;
+
+  constructor(private route: ActivatedRoute, private networkService: NetworkService, private router: Router, private meta: Meta,) {
+    this.isLoader = true
+
+
+  }
+
+
+
+
+
+  ngOnInit() {
+
+    // Access query parameters using the ActivatedRoute service
+    this.userToken = this.route.snapshot.queryParamMap.get('userToken');
+    this.systemAuthToken = this.route.snapshot.queryParamMap.get('systemAuthToken');
+    this.operator = this.route.snapshot.queryParamMap.get('operator');
+    this.eventId = this.route.snapshot.queryParamMap.get('eventId');
+    this.networkService.getAllRecordsByPost(CONFIG.authlaunch, {
+      userToken: this.userToken,
+      operatorId: this.operator,
+      systemAuthToken: this.systemAuthToken
+    })
+      .pipe(first())
+      .subscribe(
+        data => {
+
+          if (data.meta.status == true) {
+            this.accessToken = data.data.sessionId;
+
+            if (this.accessToken) {
+              localStorage.setItem('token', this.accessToken);
+              localStorage.setItem('localset', 'true');
+
+
+
+              if (this.eventId) {
+                this.networkService.goToMarketCurrent(this.eventId);
+              }
+              else if(this.eventId==null || this.eventId==''){
+                this.router.navigate([`/home`]);
+              } else {
+                this.router.navigate([`/home`]);
+              }
+
+            }
+          }
+          else {
+            this.router.navigate(['/unautherized']);
+          }
+        },
+        error => {
+          let responseData = error;
+        });
+
+
+
+
+
+
+  }
+
+
+  ngOnDestroy() {
+    this.isLoader = false
+
+  }
+}
