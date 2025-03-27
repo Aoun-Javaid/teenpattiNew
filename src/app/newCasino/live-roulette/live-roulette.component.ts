b9e60f38c8a988caecb431634d5f5f7218e30920
Maaz Bukhari <maazbukhari1144@gmail.com>
2025-03-27 03:58:35 -0700

    refactored live-roulette!

--- src/app/newCasino/live-roulette/live-roulette.component.ts
+++ src/app/newCasino/live-roulette/live-roulette.component.ts
@@ -5,19 +5,25 @@ import { ToggleService } from '../../services/toggle.service';
 import { IndexedDbService } from '../../services/indexed-db.service';
 import { ShortNumberPipe } from '../../pipes/short-number.pipe';
 import { BetCoinComponent } from '../../shared/bet-coin/bet-coin.component';
-import { TopResultsComponent } from "../shared/top-results/top-results.component";
-import { VideoPlayerComponent } from "../../shared/video-player/video-player.component";
+import { TopResultsComponent } from '../shared/top-results/top-results.component';
+import { VideoPlayerComponent } from '../../shared/video-player/video-player.component';
 
 @Component({
   selector: 'app-live-roulette',
   standalone: true,
-  imports: [CommonModule, ShortNumberPipe, BetCoinComponent, TopResultsComponent, VideoPlayerComponent],
+  imports: [
+    CommonModule,
+    ShortNumberPipe,
+    BetCoinComponent,
+    TopResultsComponent,
+    VideoPlayerComponent,
+  ],
   templateUrl: './live-roulette.component.html',
   styleUrl: './live-roulette.component.css',
 })
 export class LiveRouletteComponent {
   RoundWinner: any;
-
+  btnAnimation: any;
   coinAnimateState = false;
   animateCoinVal: any;
   btnCheck = 1;
@@ -28,6 +34,14 @@ export class LiveRouletteComponent {
   stackButtonArry: any = [];
   openCoinBarState = false;
   coinAnimateCheck = false;
+  screenWith: any;
+  btnYValue: any = '-100vh + 650px';
+  move_center_back_1: any = '168px';
+  move_center_back_2: any = '112px';
+  move_center_back_3: any = '56px';
+  move_center_back_5: any = '-56px';
+  move_center_back_6: any = '-112px';
+  btnAnimationValue: any = '168px';
   game: any;
 
   constructor(
@@ -37,6 +51,84 @@ export class LiveRouletteComponent {
 
   ngOnInit(): void {
     this.getStackData();
+    this.setAnimationsValues();
+  }
+
+  setAnimationsValues() {
+    this.screenWith = window.innerWidth;
+    if (this.screenWith >= 1024) {
+      this.btnYValue = '-100vh + 1100px';
+      this.move_center_back_1 = '322px';
+      this.move_center_back_2 = '215px';
+      this.move_center_back_3 = '109px';
+      this.move_center_back_5 = '-106px';
+      this.move_center_back_6 = '-213px';
+      this.btnAnimationValue = '322px';
+    } else if (this.screenWith >= 820 && this.screenWith < 1024) {
+      this.btnYValue = '-100vh + 1000px';
+      this.move_center_back_1 = '285px';
+      this.move_center_back_2 = '190px';
+      this.move_center_back_3 = '95px';
+      this.move_center_back_5 = '-95px';
+      this.move_center_back_6 = '-190px';
+      this.btnAnimationValue = '285px';
+    } else if (this.screenWith >= 768 && this.screenWith < 820) {
+      this.btnYValue = '-100vh + 880px';
+      this.move_center_back_1 = '249px';
+      this.move_center_back_2 = '166px';
+      this.move_center_back_3 = '83px';
+      this.move_center_back_5 = '-83px';
+      this.move_center_back_6 = '-166px';
+      this.btnAnimationValue = '249px';
+    } else if (this.screenWith >= 430 && this.screenWith > 414) {
+      this.btnYValue = '-100vh + 730px';
+      this.move_center_back_1 = '195px';
+      this.move_center_back_2 = '129px';
+      this.move_center_back_3 = '65px';
+      this.move_center_back_5 = '-65px';
+      this.move_center_back_6 = '-129px';
+      this.btnAnimationValue = '195px';
+    } else if (this.screenWith >= 414 && this.screenWith < 430) {
+      this.btnYValue = '-100vh + 700px';
+      this.move_center_back_1 = '186px';
+      this.move_center_back_2 = '124px';
+      this.move_center_back_3 = '62px';
+      this.move_center_back_5 = '-62x';
+      this.move_center_back_6 = '-124px';
+      this.btnAnimationValue = '186px';
+    }
+
+    document.documentElement.style.setProperty('--btnYValue', this.btnYValue);
+    document.documentElement.style.setProperty(
+      '--btnMatchValue',
+      this.btnAnimationValue
+    );
+
+    document.documentElement.style.setProperty(
+      '--coin-1',
+      this.move_center_back_1
+    );
+    document.documentElement.style.setProperty(
+      '--coin-2',
+      this.move_center_back_2
+    );
+    document.documentElement.style.setProperty(
+      '--coin-3',
+      this.move_center_back_3
+    );
+    document.documentElement.style.setProperty(
+      '--coin-5',
+      this.move_center_back_5
+    );
+    document.documentElement.style.setProperty(
+      '--coin-6',
+      this.move_center_back_6
+    );
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event) {
+    this.setAnimationsValues();
   }
 
   getStackData() {
@@ -54,17 +146,20 @@ export class LiveRouletteComponent {
 
   openBetCoin() {
     this.openCoinBarState = true;
+    this.btnAnimation = true;
   }
 
   closeBetCoin(event: any) {
     event.stopPropagation();
     setTimeout(() => {
       this.openCoinBarState = false;
-    }, 500);
+    }, 300);
     this.coinAnimateCheck = true;
     setTimeout(() => {
       this.coinAnimateCheck = false;
-    }, 500);
+    }, 300);
+
+    this.btnAnimation = false;
 
     let translateYRevers = ''; // Change `const` to `let`
 
