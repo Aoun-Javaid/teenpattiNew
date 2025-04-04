ab12f38dc6fc18b6cfb52744a38a13002ac65060
faizan99904 <faizan99904@gmail.com>
2025-04-04 11:01:04 +0500

    remove extra changes

--- src/app/pages/mybets/mybets.component.ts
+++ src/app/pages/mybets/mybets.component.ts
@@ -2,7 +2,6 @@ import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { BetdetailsModalComponent } from "../../Modals/betdetails-modal/betdetails-modal.component";
 import { ModalService } from '../../services/modal.service';
-import { NavigationEnd, NavigationStart, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-mybets',
@@ -13,7 +12,7 @@ import { NavigationEnd, NavigationStart, Route, Router } from '@angular/router';
 })
 export class MybetsComponent {
   activeTab='casino';
-  constructor(private modalsService: ModalService, private router:Router){
+  constructor(private modalsService:ModalService){
 
   }
   rows:any=[
@@ -40,10 +39,4 @@ export class MybetsComponent {
     }
     this.modalsService.setBetsDetailModals(obj);
   }
-
- 
-
-
-
-
 }
